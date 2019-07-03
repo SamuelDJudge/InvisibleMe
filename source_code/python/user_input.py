ade7822644b4a84e2f39a3ef61c071c40223aa26
Samuel_Judge <Samuel.D.Judge@gmail.com>
2019-07-02 20:57:01 -0400

    Update user_input.py

--- source_code/python/user_input.py
+++ source_code/python/user_input.py
@@ -6,7 +6,7 @@ line = line.split(',')
 column_list_str = line[4:]
 column_list = []
 for a_string in column_list_str:
-    column_list.append(int(string))
+    column_list.append(int(a_string))
 file_path = str(line[0])
 write_path = str(line[1])
 en_or_de = str(line[2])
